5d0779d685bf246f32d6e206b0a61b6f4bf425c1
Yehia Khalil <yehiakhalilykk@gmail.com>
2024-04-07 21:44:24 +0200

    added Cat file

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require("zlib")
 
 // Uncomment this block to pass the first stage
 const command = process.argv[2];
@@ -8,8 +9,28 @@ switch (command) {
   case "init":
     createGitDirectory();
     break;
+  case "cat-file":
+    getCatFile();
+    break;
   default:
     throw new Error(`Unknown command ${command}`);
+}
+
+function getCatFile() {
+  const type = process.argv[3];
+  switch (type) {
+    case "-p":
+      let filePath = process.argv[4];
+      let dir = filePath.substring(0, 2);
+      let file = filePath.substring(2);
+      const contents = fs.readFileSync(path.join(__dirname, '.git/objects', dir, file));
+      const decompress = zlib.inflateSync(contents);
+      const content = decompress.toString('utf-8').split('\0')[1];
+      process.stdout.write(content);
+      break;
+    default:
+      throw new Error(`Unknown command ${command}`);
+  }
 }
 
 function createGitDirectory() {
